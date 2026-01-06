177241accecce250ae48e682454c6513f2c6576c
Prachi Joshi <prachijoshi469@gmail.com>
2026-01-06 21:09:23 +0530

    auto popup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,38 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import AutoPopupForm from "@/components/common/AutoPopupForm";
 
 export const metadata: Metadata = {
-  title: "Avexra AI",
-icons: {
-    icon: "/icons/favicon.ico",
-  },
-  description: "Intelligent Systems That Work For You",
+	title: "Avexra AI",
+	icons: {
+		icon: "/icons/favicon.ico",
+	},
+	description: "Intelligent Systems That Work For You",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
-          rel="stylesheet"
-        />
+	return (
+		<html lang="en">
+			<head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
+					rel="stylesheet"
+				/>
 
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
+					rel="stylesheet"
+				/>
+			</head>
 
-      <body className="bg-background-light text-text-main font-sans antialiased selection:bg-primary selection:text-white overflow-x-visible">
-        {children}
-      </body>
-    </html>
-  );
+			<body className="bg-background-light text-text-main font-sans antialiased selection:bg-primary selection:text-white overflow-x-visible">
+				{children}
+				<AutoPopupForm />
+			</body>
+		</html>
+	);
 }
